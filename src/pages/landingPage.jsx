4fed47d6ffa0bdbae518d7f3702a0a47abc2c04c
Paupito <paula.frias@ug.uchile.cl>
2020-09-29 08:45:34 -0300

    creacion del footer

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -1,12 +1,14 @@
 import React, { Fragment } from "react";
 import Header from '../components/landing/header';
 import Video from '../components/landing/video';
+import Footer from '../components/landing/footer';
 
 const LandingPage = () => {
   return (
     <Fragment>
       <Header />
       <Video />
+      <Footer/>
     </Fragment>
   );
 }

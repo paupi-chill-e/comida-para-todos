8bb4fc78047b42b0a35522439688955edad40441
lady pino <ladypino112@gmail.com>
2020-10-06 04:02:36 -0300

    changes landing pages

--- src/components/landing/footer.jsx
+++ src/components/landing/footer.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import './styles/footer.css';
 import logox from '../../img/logo.png'
-import chems from '../../img/chemspng.png';
+import  Ig from '../../img/ig.png';
+import Linked from '../../img/linked.png';
+import Mail from '../../img/mail.png';
 
 function Footer() {
   return (
@@ -11,14 +14,30 @@ function Footer() {
           <img src={logox} alt="" className='logoFooter' />
         </div>
         <div className='containerIconsFooter'>
-          <img src={chems} alt="" className='iconFooter' />
+          <img src={Ig} alt="" className='iconFooter' />
+          <img src={Linked} alt="" className='iconFooter' />
+          <img src={Mail} alt="" className='iconFooter' />
         </div>
       </div>
       <div className='infoFooter'>
-        <p>holamundo</p>
-        <p>piterciosnorompaslacama</p>
-        <p>porfavor</p>
-      </div>
+      <div className='descriptionMotivation'>
+        <p>• Quiénes somos</p>
+        </div>
+        <div className='descriptionMotivation'>
+        <p >• Transparencia</p>
+        </div>
+        <div className='descriptionMotivation'>
+        <p >• Qué hacemos</p>
+        </div>
+        <div className='descriptionMotivation'>
+        <p>• Cómo ayudar</p>
+        </div>
+        </div>
+      <div className= 'btnFootertwo'>
+      <Link to="/">
+          <button className="btnFooter">Hazte Parte</button>
+        </Link>
+        </div>
     </footer>
   );
 }

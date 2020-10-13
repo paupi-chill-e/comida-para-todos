1042f1312e8d2bcf062cb76ae5c2a9765ee43098
lady pino <ladypino112@gmail.com>
2020-10-13 08:06:09 -0300

    finish integration

--- src/components/landing/activitySection.jsx
+++ src/components/landing/activitySection.jsx
@@ -29,26 +29,27 @@ import y from '../../img/Landing/Alianzas/4.jpg';
 import z from '../../img/Landing/Alianzas/3.jpg';
 import ch from '../../img/Landing/Alianzas/2.jpg';
 import ll from '../../img/Landing/Alianzas/1.jpg';
-
 import "./styles/activitySectionF.css";
-import Instagram from '../../img/instagram.png'
+
 const ActivitySectionF = () => {
   return (
       <Fragment>
+
+    <div className="containerActivitySection">
     <div className='textActivitySection'>
     <strong>Actividad Resiente</strong>
-
-     {/* instagram renderizado de ultimas img directo de la ap redirige y se actualiza cada 24 horas de forma automatica con la ultima informacion  */}
-         {/* <iframe class="instagram-media-instagram-media-rendered" id="instagram-embed-0" src="https://www.instagram.com/p/CGN_2tcHIk4/embed/?cr=1&amp;v=12&amp;wp=746&amp;rd=http%3A%2F%2Flocalhost%3A3000&amp;rp=%2FDonaciones#%7B%22ci%22%3A0%2C%22os%22%3A1508.6749999900348%2C%22ls%22%3A665.4649999982212%2C%22le%22%3A667.650000017602%7D ;"></iframe>  */}
-        
-        
-        {/*instagram renderizado de ultimas imgs directo de la ap redirige,publica,likes  */}          
- <iframe className='renderautomatic' src="https://www.powr.io/instagram-feed/u/c349a139_1602450916#platform=embed" ></iframe> 
-      
-  
-
+{""}
+     {/*          
+         instagram renderizado de ultimas imgs directo de la ap redirige,publica,likes 
+     <iframe className="renderautomatic" id="instagram-embed-0" src="https://www.instagram.com/p/CGQbzXknyKD/embed/?cr=1&amp;v=12&amp;wp=746&amp;rd=http%3A%2F%2Flocalhost%3A3000&amp;rp=%2F#%7B%22ci%22%3A0%2C%22os%22%3A2414.7549999997864%2C%22ls%22%3A1063.8099999996484%2C%22le%22%3A1066.3750000003347%7D;"></iframe>*/} 
+     
+         instagram renderizado de ultimas img directo de la ap redirige y se actualiza cada 24 horas de forma automatica con la ultima informacion
+ <iframe className='instagram-media-instagram-media-rendered' src="https://www.powr.io/instagram-feed/u/c349a139_1602450916#platform=embed" ></iframe> 
+       
     </div>
-    <div className="containerActivitySection">
+   
+
+
     <strong>Nuestros Colaboradores</strong>
     <Carousel autoPlay showThumbs={false} infiniteLoop={true}>
         <div className='eachColab'>
